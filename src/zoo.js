cdc67cc361548bd7468235248bc56cc5dc0d15e6
Janaina P de Oliveira Minutti <janainapp@gmail.com>
2021-04-09 16:45:52 -0300

    requisito 2

--- src/zoo.js
+++ src/zoo.js
@@ -15,70 +15,82 @@ const { animals } = data;
 
 // (...ids) busca espécies de animais por ids, independente de quantos sejam.
 // filter fará novo array de acordo com as condições true do some.
+// para concluir este requisito precisei de auxilio do colega Emerson Saturnino, pois meu código não estava //
+// funcionando como o esperado no avaliador
+
 function animalsByIds(...ids) {
   return animals.filter((animal) =>
     ids.some((verifyId) => animal.id === verifyId));
 }
 
-/* function animalsOlderThan(animal, age) {
-  // seu código aqui
-}
+// testa se todos os animais desta espécie possuem a idade mínima especificada
+// retorna boleano
+// every verifica se todos os itens atendem a condição especificada.
 
-function employeeByName(employeeName) {
-  // seu código aqui
+function animalsOlderThan(animalName, age) {
+  const animalVerify = animals.find((animal) => animal.name === animalName);
+  return animalVerify.residents.every((resident) => resident.age >= age);
 }
+console.log(animalsOlderThan('otters', 7));
 
-function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
-}
+// busca das pessoas colaboradoras através do primeiro ou do último nome delas
+// filter fará novo array de acordo com as condições true do some.
+// function employeeByName(employeeName) {
+//   return employees.filter((name) =>
+//     employeeName.some((verifyName) => employees.firstName || employees.lastName === verifyName));
+// }
 
-function isManager(id) {
-  // seu código aqui
-}
+// function createEmployee(personalInfo, associatedWith) {
+//   // seu código aqui
+// }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
-}
+// function isManager(id) {
+//   // seu código aqui
+// }
 
-function animalCount(species) {
-  // seu código aqui
-}
+// function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+//   // seu código aqui
+// }
 
-function entryCalculator(entrants) {
-  // seu código aqui
-}
+// function animalCount(species) {
+//   // seu código aqui
+// }
 
-function animalMap(options) {
-  // seu código aqui
-}
+// function entryCalculator(entrants) {
+//   // seu código aqui
+// }
 
-function schedule(dayName) {
-  // seu código aqui
-}
+// function animalMap(options) {
+//   // seu código aqui
+// }
 
-function oldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+// function schedule(dayName) {
+//   // seu código aqui
+// }
 
-function increasePrices(percentage) {
-  // seu código aqui
-}
+// function oldestFromFirstSpecies(id) {
+//   // seu código aqui
+// }
 
-function employeeCoverage(idOrName) {
-  // seu código aqui
-}
-*/
+// function increasePrices(percentage) {
+//   // seu código aqui
+// }
+
+// function employeeCoverage(idOrName) {
+//   // seu código aqui
+// }
+
 module.exports = {
 //   entryCalculator,
 //   schedule,
 //   animalCount,
 //   animalMap,
   animalsByIds,
-//   employeeByName,
-//   employeeCoverage,
-//   addEmployee,
-//   isManager,
-//   animalsOlderThan,
+  // employeeByName,
+  //   employeeCoverage,
+  //   addEmployee,
+  //   isManager,
+  animalsOlderThan,
 //   oldestFromFirstSpecies,
 //   increasePrices,
 //   createEmployee,
