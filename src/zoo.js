9cfcfebe7b880d44da99a6f341edfbeca083f963
Janaina P de Oliveira Minutti <janainapp@gmail.com>
2021-04-09 11:45:32 -0300

    correção requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -16,10 +16,9 @@ const { animals } = data;
 // (...ids) busca espécies de animais por ids, independente de quantos sejam.
 // filter fará novo array de acordo com as condições true do some.
 function animalsByIds(...ids) {
-  return animals.filter((animal) => 
-    ids.some((verifyId) => animal.id === verifyId));
+return animals.filter((animal) => 
+  ids.some((verifyId) => animal.id === verifyId));
 }
-console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'));
 
 /* function animalsOlderThan(animal, age) {
   // seu código aqui
